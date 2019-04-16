cbeb649dca1d102f2a2a51ba0943d52a2495fea5
Katalam <goerss99@gmail.com>
2019-04-17 00:25:19 +0200

    Added tfar_tape

---
+++ addons/tfar_tape/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT tfar_tape
+#define COMPONENT_BEAUTIFIED TFARTape
+#include "\x\kat_10thmods\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_TFAR_TAPE
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_TFAR_TAPE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_TFAR_TAPE
+#endif
+
+#include "\x\kat_10thmods\addons\main\script_macros.hpp"
