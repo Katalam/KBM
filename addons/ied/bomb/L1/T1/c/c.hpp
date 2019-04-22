067c841bbded30eafacea9e280b58512b28c2a1c
Katalam <goerss99@gmail.com>
2019-04-22 14:48:51 +0200

    Circle ci summary

--- addons/ied/bomb/L1/T1/c/c.hpp
+++ addons/ied/bomb/L1/T1/c/c.hpp
@@ -1,52 +1,52 @@
 class GVAR(L1_T1_c) {
-	idd = -1;
-	movingEnable = false;
-	onLoad = "";
-	onUnload = "1 cutText ['', 'PLAIN']";
-	class Controls {
-		class backround: GVAR(L1_T1_Picture) {
-			idc = 1200;
-			style = 2096;
-			text = QPATHTOF(bomb\L1\T1\c\armed.paa);
-			x = safeZoneX;
-			y = safeZoneY;
-			w = safeZoneW;
-			h = safeZoneH;
-		};
-		class wire: GVAR(L1_T1_CutWire) {
-			idc = 1600;
-			text = "";
-			x = safeZoneX + 0.666 * safeZoneW;
-			y = safeZoneY + 0.308 * safeZoneH;
-			w = safeZoneW * 0.017;
-			h = safeZoneH * 0.017;
-		};
-		class anywhere: GVAR(L1_T1_ClickAnywhere) {
-			idc = 1601;
-			text = "";
-			x = safeZoneX + 0.30 * safeZoneW;
-			y = safeZoneY + 0.26 * safeZoneH;
-			w = safeZoneW * 0.4;
-			h = safeZoneH * 0.4;
-		};
-	};
+    idd = -1;
+    movingEnable = false;
+    onLoad = "";
+    onUnload = "1 cutText ['', 'PLAIN']";
+    class Controls {
+        class backround: GVAR(L1_T1_Picture) {
+            idc = 1200;
+            style = 2096;
+            text = QPATHTOF(bomb\L1\T1\c\armed.paa);
+            x = safeZoneX;
+            y = safeZoneY;
+            w = safeZoneW;
+            h = safeZoneH;
+        };
+        class wire: GVAR(L1_T1_CutWire) {
+            idc = 1600;
+            text = "";
+            x = safeZoneX + 0.666 * safeZoneW;
+            y = safeZoneY + 0.308 * safeZoneH;
+            w = safeZoneW * 0.017;
+            h = safeZoneH * 0.017;
+        };
+        class anywhere: GVAR(L1_T1_ClickAnywhere) {
+            idc = 1601;
+            text = "";
+            x = safeZoneX + 0.30 * safeZoneW;
+            y = safeZoneY + 0.26 * safeZoneH;
+            w = safeZoneW * 0.4;
+            h = safeZoneH * 0.4;
+        };
+    };
 };
 
 // Disarmed
 class GVAR(L1_T1_c_Disarmed) {
-	idd = -1;
-	movingEnable = false;
-	onLoad = "";
-	onUnload = "1 cutText ['', 'PLAIN']";
-	class Controls {
-		class backround: GVAR(L1_T1_Picture) {
-			idc = 1200;
-			style = 2096;
-			text = QPATHTOF(bomb\L1\T1\c\disarmed.paa);
-			x = safeZoneX;
-			y = safeZoneY;
-			w = safeZoneW;
-			h = safeZoneH;
-		};
-	};
+    idd = -1;
+    movingEnable = false;
+    onLoad = "";
+    onUnload = "1 cutText ['', 'PLAIN']";
+    class Controls {
+        class backround: GVAR(L1_T1_Picture) {
+            idc = 1200;
+            style = 2096;
+            text = QPATHTOF(bomb\L1\T1\c\disarmed.paa);
+            x = safeZoneX;
+            y = safeZoneY;
+            w = safeZoneW;
+            h = safeZoneH;
+        };
+    };
 };
--- addons/ied/bomb/L1/T1/f/f.hpp
+++ addons/ied/bomb/L1/T1/f/f.hpp
@@ -1,52 +1,52 @@
 class GVAR(L1_T1_f) {
-	idd = -1;
-	movingEnable = false;
-	onLoad = "";
-	onUnload = "1 cutText ['', 'PLAIN']";
-	class Controls {
-		class backround: GVAR(L1_T1_Picture) {
-			idc = 1200;
-			style = 2096;
-			text = QPATHTOF(bomb\L1\T1\f\armed.paa);
-			x = safeZoneX;
-			y = safeZoneY;
-			w = safeZoneW;
-			h = safeZoneH;
-		};
-		class wire: GVAR(L1_T1_CutWire) {
-			idc = 1600;
-			text = "";
-			x = safeZoneX + 0.666 * safeZoneW;
-			y = safeZoneY + 0.446 * safeZoneH;
-			w = safeZoneW * 0.017;
-			h = safeZoneH * 0.017;
-		};
-		class anywhere: GVAR(L1_T1_ClickAnywhere) {
-			idc = 1601;
-			text = "";
-			x = safeZoneX + 0.30 * safeZoneW;
-			y = safeZoneY + 0.26 * safeZoneH;
-			w = safeZoneW * 0.4;
-			h = safeZoneH * 0.4;
-		};
-	};
+    idd = -1;
+    movingEnable = false;
+    onLoad = "";
+    onUnload = "1 cutText ['', 'PLAIN']";
+    class Controls {
+        class backround: GVAR(L1_T1_Picture) {
+            idc = 1200;
+            style = 2096;
+            text = QPATHTOF(bomb\L1\T1\f\armed.paa);
+            x = safeZoneX;
+            y = safeZoneY;
+            w = safeZoneW;
+            h = safeZoneH;
+        };
+        class wire: GVAR(L1_T1_CutWire) {
+            idc = 1600;
+            text = "";
+            x = safeZoneX + 0.666 * safeZoneW;
+            y = safeZoneY + 0.446 * safeZoneH;
+            w = safeZoneW * 0.017;
+            h = safeZoneH * 0.017;
+        };
+        class anywhere: GVAR(L1_T1_ClickAnywhere) {
+            idc = 1601;
+            text = "";
+            x = safeZoneX + 0.30 * safeZoneW;
+            y = safeZoneY + 0.26 * safeZoneH;
+            w = safeZoneW * 0.4;
+            h = safeZoneH * 0.4;
+        };
+    };
 };
 
 // Disarmed
 class GVAR(L1_T1_f_Disarmed) {
-	idd = -1;
-	movingEnable = false;
-	onLoad = "";
-	onUnload = "1 cutText ['', 'PLAIN']";
-	class Controls {
-		class backround: GVAR(L1_T1_Picture) {
-			idc = 1200;
-			style = 2096;
-			text = QPATHTOF(bomb\L1\T1\f\disarmed.paa);
-			x = safeZoneX;
-			y = safeZoneY;
-			w = safeZoneW;
-			h = safeZoneH;
-		};
-	};
+    idd = -1;
+    movingEnable = false;
+    onLoad = "";
+    onUnload = "1 cutText ['', 'PLAIN']";
+    class Controls {
+        class backround: GVAR(L1_T1_Picture) {
+            idc = 1200;
+            style = 2096;
+            text = QPATHTOF(bomb\L1\T1\f\disarmed.paa);
+            x = safeZoneX;
+            y = safeZoneY;
+            w = safeZoneW;
+            h = safeZoneH;
+        };
+    };
 };
