fa94614c03776e17d65fad3861a87c58f9e986d1
Katalam <goerss99@gmail.com>
2019-04-26 15:08:53 +0200

    Added acre tape to optionals

---
+++ optionals/acre_tape/Prc152_RadioDialog.hpp
@@ -0,0 +1,6 @@
+class Prc152_RadioDialog {
+    class Prc152_RscBackground;
+    class Prc152Background: Prc152_RscBackground {
+        text = QPATHTOF(ui\prc152c_ui.paa);
+    };
+};
---
+++ optionals/acre_tape/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT acre_tape
+#define COMPONENT_BEAUTIFIED ACRE Tape
+#include "\x\kat_10thmods\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_ACRE_TAPE
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_ACRE_TAPE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_ACRE_TAPE
+#endif
+
+#include "\x\kat_10thmods\addons\main\script_macros.hpp"
