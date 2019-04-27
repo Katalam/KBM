59288d89b1b4b44baae4fa09cbec28fc7e064727
Katalam <goerss99@gmail.com>
2019-04-27 04:16:45 +0200

    Added dummy

---
+++ extras/dummy/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT dummy
+#define COMPONENT_BEAUTIFIED Dummy
+#include "\x\kat_10thmods\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_DUMMY
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_DUMMY
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_DUMMY
+#endif
+
+#include "\x\kat_10thmods\addons\main\script_macros.hpp"
