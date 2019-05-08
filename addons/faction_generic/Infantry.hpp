7e3dd31565e86b592af07bd3bede3f25da3bf2b8
Katalam <goerss99@gmail.com>
2019-05-08 04:33:48 +0200

    More medic material for everyone

--- addons/faction_generic/Infantry.hpp
+++ addons/faction_generic/Infantry.hpp
@@ -31,7 +31,7 @@ class GVAR(USAM_RFM): B_Soldier_F {
                         {"rhsusf_weap_m9", {"rhsusf_mag_15Rnd_9x19_FMJ"}}, // handgun
                         {"", {}}}; // binocular
     loadoutLinked[] = {"ItemMap", "ItemCompass", "ItemWatch", "TFAR_anprc152", "ItemMicroDAGR"};
-    loadoutMedical[] = {{"ACE_packingBandage", 8}, {"ACE_elasticBandage", 5}, {"ACE_tourniquet", 2}, {"adv_aceSplint_splint", 2}, {"ACE_morphine", 1}};
+    loadoutMedical[] = {{"ACE_packingBandage", 10}, {"ACE_elasticBandage", 8}, {"ACE_tourniquet", 2}, {"adv_aceSplint_splint", 2}, {"ACE_morphine", 1}}; // TODO Painkillers , {"KAT_Painkiller", 1}
     loadoutNVG[] = {"ACE_NVG_Wide", {{"ACE_Flashlight_XL50", 1}, {"ACE_IR_Strobe_Item", 1}}, {{"Chemlight_red", 3}}};
     loadoutGrenade[] = {{"rhs_mag_an_m8hc", 4}, {"rhs_mag_mk84", 4}, {"HandGrenade", 2}};
     loadoutUniformInv[] = {{"ACE_MapTools", 1}, {"ACE_microDAGR", 1}, {"ACE_CableTie", 2}};
