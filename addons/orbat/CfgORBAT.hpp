ad196075dea8d7a4d9b3835a8cbc4c53d8eabcfe
Katalam <goerss99@gmail.com>
2019-06-06 15:51:53 +0200

    Fixed building orbat

--- addons/orbat/CfgORBAT.hpp
+++ addons/orbat/CfgORBAT.hpp
@@ -1,6 +1,10 @@
 class CfgORBAT {
-    #include "10md.hpp" // configFile >> "CfgORBAT" >> "AC", configFile >> "CfgORBAT" >> "MD"
-    #include "3id.hpp" // configFile >> "CfgORBAT" >> "ID"
-    #include "101ad.hpp" // configFile >> "CfgORBAT" >> "AD"
-    #include "csg2.hpp" // configFile >> "CfgORBAT" >> "CSG"
+    #include "10md.hpp"
+    #include "3id.hpp"
+    #include "101ad.hpp"
+    #include "csg2.hpp"
 };
+// configFile >> "CfgORBAT" >> "AC", configFile >> "CfgORBAT" >> "MD"
+// configFile >> "CfgORBAT" >> "ID"
+// configFile >> "CfgORBAT" >> "AD"
+// configFile >> "CfgORBAT" >> "CSG"
