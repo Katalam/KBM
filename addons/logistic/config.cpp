c8026a3fb7f699db3a1247dd58df5f2fda888dcb
Katalam <goerss99@gmail.com>
2019-08-16 16:42:06 +0200

    Update config.cpp

--- addons/logistic/config.cpp
+++ addons/logistic/config.cpp
@@ -4,7 +4,19 @@ class CfgPatches {
     class ADDON {
         name = QUOTE(COMPONENT_NAME);
         units[] = {
-            QGVAR(moduleAddLogisticMenu3den)
+            QGVAR(moduleAddLogisticMenu3den),
+            QGVAR(USAM_Crate_INF),
+            QGVAR(USAM_Crate_EMP),
+            QGVAR(USAM_Crate_FRAG),
+            QGVAR(USAM_Crate_ESP),
+            QGVAR(USAM_Crate_LAT),
+            QGVAR(USAM_Crate_HAT),
+            QGVAR(USAM_Crate_WPN),
+            QGVAR(USAM_Crate_MMG),
+            QGVAR(USAM_Crate_MED),
+            QGVAR(USAM_Crate_MOR_HE),
+            QGVAR(USAM_Crate_MOR_SMK),
+            QGVAR(USAM_Crate_FULL)
         };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
