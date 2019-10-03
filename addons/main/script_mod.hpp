b39686a7b133c4d762e9ccff8cb4c0b8a1541e63
Katalam <goerss99@gmail.com>
2019-10-03 17:52:17 +0200

    Cleaning things

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -1,22 +1,9 @@
-// COMPONENT should be defined in the script_component.hpp and included BEFORE this hpp
-
 #define MAINPREFIX z
 #define PREFIX kat_10thmods
 
-#define MAJOR 1
-#define MINOR 0
-#define PATCHLVL 0
-#define BUILD 0
+#include "script_version.hpp"
 
-#define VERSION MAJOR.MINOR.PATCHLVL.BUILD
-#define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
+#define VERSION MAJOR.MINOR.PATCH.BUILD
+#define VERSION_AR MAJOR,MINOR,PATCH,BUILD
 
-// MINIMAL required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 1.88
-#define REQUIRED_CBA_VERSION {3,8,0}
-
-#ifdef COMPONENT_BEAUTIFIED
-    #define COMPONENT_NAME QUOTE(KAT - COMPONENT_BEAUTIFIED)
-#else
-    #define COMPONENT_NAME QUOTE(KAT - COMPONENT)
-#endif
--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
-#define MAJOR 1
+#define MAJOR 0
 #define MINOR 1
-#define PATCHLVL 6
-#define BUILD 32
+#define PATCH 0
+#define BUILD 0
